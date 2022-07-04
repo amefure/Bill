95c46d082d9b9f03cf9e91380a7c949eec9fe73d
ame <shachi10969@gmail.com>
2022-07-04 21:28:13 +0900

    Initial Commit

---
+++ bill/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bill
+//
+//  Created by t&a on 2022/07/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bill/billApp.swift
@@ -0,0 +1,17 @@
+//
+//  billApp.swift
+//  bill
+//
+//  Created by t&a on 2022/07/04.
+//
+
+import SwiftUI
+
+@main
+struct billApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
